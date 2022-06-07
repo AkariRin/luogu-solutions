908afa69863225b4c723490f0be13a1acdad0de3
AkariRin <akaririn1028@gmail.com>
2022-06-07 19:12:20 +0800

    B2125

---
+++ B2125.py
@@ -0,0 +1,16 @@
+c = int(input())
+classmate = dict()
+i = 0
+while i < c:
+    arg = input().split(' ')
+    classmate[arg[1]] = int(arg[0])
+    i += 1
+
+max_score = 0
+first = ''
+for key,value in classmate.items():
+    if value > max_score:
+        max_score = value
+        first = key
+
+print(first)
